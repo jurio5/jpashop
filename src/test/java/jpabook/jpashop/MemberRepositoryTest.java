54568f876bbea4f5e7abf269d6dd04f68b2dd9e1
jurio5 <sia5492@naver.com>
2025-06-09 03:14:10 +0900

    [chor] 테스트 케이스 임포트 재점검

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
