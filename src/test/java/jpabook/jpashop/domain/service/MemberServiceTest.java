00dff6e5783dd73eab50de3f322646912e29fabd
jurio5 <sia5492@naver.com>
2025-06-10 00:22:40 +0900

    [feat] 회원가입 & 중복 닉네임 검증 테스트 코드 추가 (테스트 코드 격리성 설정 (임베디드 메모리에서 실행))

---
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("이미 존재하는 회원입니다.");
+
+        // then
+        assertThat(memberRepository.findAll()).hasSize(1);
+    }
+}
