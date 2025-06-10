6dc41478e353bc844f03d3b191c012d235f05ca0
jurio5 <sia5492@naver.com>
2025-06-11 00:43:50 +0900

    [feat] 상품 비즈니스 로직 테스트 케이스 추가

---
+++ src/test/java/jpabook/jpashop/domain/service/ItemServiceTest.java
@@ -0,0 +1,66 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class ItemServiceTest {
+
+    @Autowired
+    ItemService itemService;
+
+    @Test
+    void 아이템_저장() {
+        // given
+        Book book = new Book();
+        book.setAuthor("김영한");
+        book.setIsbn("123456789");
+
+        // when
+        itemService.saveItem(book);
+
+        // then
+        assertThat(book.getAuthor()).isEqualTo("김영한");
+        assertThat(book.getIsbn()).isEqualTo("123456789");
+    }
+
+    @Test
+    void 상품_다건_조회() {
+        // given
+        Book book1 = new Book();
+        book1.setAuthor("김영한");
+        book1.setIsbn("123456789");
+
+        Book book2 = new Book();
+        book2.setAuthor("박영준");
+        book2.setIsbn("789456123");
+
+        // when
+        itemService.saveItem(book1);
+        itemService.saveItem(book2);
+
+        // then
+        assertThat(itemService.findItems()).hasSize(2);
+    }
+
+    @Test
+    void 상품_단건_조회() {
+        // given
+        Book book = new Book();
+        book.setAuthor("김영한");
+        book.setIsbn("1234567890");
+
+        // when
+        itemService.saveItem(book);
+
+        // then
+        assertThat(itemService.findOne(book.getId())).isEqualTo(book);
+    }
+
+}
